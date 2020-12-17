653800b703db01c7c738b988ded2ec3881d6eb15
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-17 00:24:17 +0000

    good

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Javascript that calculates the area of a circle.
 
-document.getElementById('submitbutton').addEventListener('click', areaCalculation)
+document.getElementById('submitbutton').addEventListener('click', areacircumfrenceCalculation)
 
-function areaCalculation () {
+function areacircumfrenceCalculation () {
   const radius = document.getElementById('radius').value
   const radiussquared = radius * radius
   const areatotal = radiussquared * 3.14
   document.getElementById('area').innerHTML = areatotal
+  const circumfrence = 6.28318530718 * radius
+  const circumfrencetotal = circumfrence.toFixed(2)
+  document.getElementById('circumfrence').innerHTML = circumfrencetotal
 }
